b527b10d83fb1c104a013e594e985f93adb8afde
mohammedfaazk <mohammedfaaz.26csb@licet.ac.in>
2025-07-29 14:56:36 +0530

    Revert "Initial commit"
    This reverts commit 99c79a79177d846cc285b0e0e9ba4674be988700.

--- src/monitors/BaseMonitor.ts
+++
@@ -1,6 +0,0 @@
-import { EventEmitter } from 'events';
-
-export abstract class BaseMonitor extends EventEmitter {
-  abstract start(): Promise<void>;
-  abstract stop(): Promise<void>;
-}
--- src/types/resolver.ts
+++
@@ -1,8 +0,0 @@
-export interface ResolverInfo {
-  address: string;
-  endpoint: string;
-  stake: string;       // ETH amount staked as string
-  reputation: number;  // Reputation score
-  successfulSwaps: number;
-  totalSwaps: number;
-}
